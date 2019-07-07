87cfd58505c603b9568eed37b7dfb6b7f15c06a3
Krish Singal <krish.singal2002@gmail.com>
2019-07-07 16:02:21 -0500

    Update AmazonDataset.py

--- AmazonDataset.py
+++ AmazonDataset.py
@@ -143,6 +143,30 @@ def test_dataset(data_path='/home/cl67/workspace/se/Amazon-Product-Search-Datase
     return AmazonDataset(data_path)
 
 
+def TopMatch(query, file_path):
+    map = {}
+    top_list = []
+
+    with open(file_path) as tf:
+        for line in tf:
+            items = line.split(',')
+
+            if query == items[0]:
+                if items[1] in map:
+                    x = map.get(items[1])
+                    map[items[1]] = x + 1
+                else:
+                    map[items[1]] = 1
+
+    list = [v[0] for v in sorted(map.items(), key=lambda kv: (-kv[1], kv[0]))]
+
+    for i in range(min(100, len(list))):
+        top_list.append((list[i], map.get(list[i])))
+    # print(len(list))
+    # ret = list[:min(100, len(list))]
+    return top_list
+
+
 def main():
     #train_set = train_dataset()
     test_set = test_dataset()
